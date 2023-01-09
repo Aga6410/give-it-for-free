5e0681feaad58199be7e3d8e658a23bce7147289
aga.trybala1@gmail.com <aga.trybala1@gmial.com>
2023-01-09 22:52:36 +0100

    folders added

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+               Nawigacja 
+            </div>
+        );
+    }
+}
+
+export default Navigation;
