22270dc952b3c4f2acf0eee84551bb7d7cf18c26
aga.trybala1@gmail.com <aga.trybala1@gmial.com>
2023-01-16 12:04:14 +0100

    attempt to add pagination, not work properly

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,50 @@
 import React from 'react'
 import HomeHeader from './HomeHeader';
+import {Link} from "react-router-dom";
+import decoration from "../assets/Decoration.svg"
 
 const Login = () => {
   return (
-    <>
-    <HomeHeader/>
-    <div>Tu bedzie login</div>
-    </>
+    <div className="Login">
+      <HomeHeader/>
+      <div className="Login__main">
+        <span className="Login__logIn">Zaloguj się</span>
+        <img src={decoration} alt="decoration"/>
+
+        <form className="Login__form">
+          <div className="Login__data">
+            <label>
+              Email
+              <input
+                
+                type="email"
+                name="email"
+                id="name"
+                
+              />
+            <p ></p>
+            </label>
+            <label>
+              Hasło
+              <input
+                
+                type="password"
+                name="password"
+                id="password"
+                
+              />
+            <p className="Login_errorText"></p>
+            </label>
+          </div>
+          <div className="Login__btns">
+            <Link to="/register" className="btn">
+              Załóż konto
+            </Link>
+            <input className="btn" type="submit" value="Zaloguj się" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
