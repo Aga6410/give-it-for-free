3dceff29bf39a0de781bdbbd5050923727487663
aga.trybala1@gmail.com <aga.trybala1@gmial.com>
2023-01-23 18:45:40 +0100

    steps to do layout 1

--- src/components/StepsToDo/StepsToDo.js
+++ src/components/StepsToDo/StepsToDo.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 export default function StepsToDo() {
   return (
-    <div className= "StepsToDo_ conta">
+    <div className= "StepsToDo_container container">
+      <div className="importantInformation">
+        <h1>Important!</h1>
+        <p>Fill in the details about your things. Thanks to that we know who we can give them.</p>
+      </div>
+        <div className="firstStep step">
+          <p>Step 1/4</p>
+          <h1>Choose what you would like to give away: </h1>
+          <input type="radio" value="ubrania, które nadają się do ponownego użycia" name="return" />
+          <label>second hand clothes</label><br/>
+          <input type="radio" name="return" value="ubrania, do wyrzucenia" />
+          <label>cloth to throw away</label><br/>
+          <input type="radio" name="return" value="zabawki" />
+          <label>toys</label><br/>
+          <input type="radio" name="return" value="książki" />
+          <label>books</label><br/>
+          <input type="radio" name="return" value="inne" />
+          <label>others</label>                
+        </div> 
+        <button>Next</button>   
+                 
+      
+                
 
     </div>
   )
