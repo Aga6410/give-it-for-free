44798adee76bc5a72873bc517fc46bdeecea7c40
aga.trybala1@gmail.com <aga.trybala1@gmial.com>
2023-01-26 19:35:05 +0100

    register + login work

--- src/components/Logout-login-register/Register.js
+++ src/components/Logout-login-register/Register.js
@@ -1,141 +1,3 @@
-// import React from 'react'
-// import HomeHeader from './Header/HomeHeader';
-// import decoration from "../../assets/Decoration.svg";
-// import {Link} from "react-router-dom";
-// import {useState} from 'react';
-// import { useSignup } from '../Hooks/useSignup'
-
-
-// const Register = () => {
-
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [passwordRep, setPasswordRep] = useState('');
-//   const {signup, isPending, error } = useSignup();
-
-//   const [form, setForm] = useState({
-//     email: "",
-//     password: "",
-//     passwordRep: ""
-//   });
-//   const [errors, setErrors] = useState({
-//     email: "",
-//     password: "",
-//     passwordRep: ""
-//   });
-
-//   const updateForm = e => {
-//     const { name, value } = e.target;
-//     setForm({ ...form, [name]: value });
-
-//     setErrors(prevState => ({
-//       ...prevState,
-//       [name]: ""
-//     }));
-//   };
-
-//   // const handleChangeEmail = (e) => {
-//   //   setEmail(e.target.value);
-//   // }
-  
-//   // const handleChangePassword = (e) => {
-//   //   setPassword(e.target.value);
-//   // }
-
-//   // const handleChangePasswordRep = (e) => {
-//   //   setPasswordRep(e.target.value);
-//   // }
-
-//   const validate = () => {
-
-//     const err = {};
-//     const re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-
-//     if (!re.test(form.email)) {
-//       err.email = "The email is badly formated!";
-//     }
-
-//     if (form.password.length < 6) {
-//       err.password = "The password is too short!";
-//     }
-
-//     if (form.passwordRep !== form.password) {
-//       err.passwordRep = "The passwords are not the same!";
-//     }
-
-//     if (Object.values(err).find(e => e.length)) {
-//       setErrors(err);
-//       return false;
-//     }
-//     return true;
-//   };
-  
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (validate()) {
-//     signup(email, password, passwordRep)
-//     }
-//   }
-
-//   return (
-//     <div className="Register_container container">
-//       <HomeHeader/>
-//       <div className="Register__main">
-//         <span className="Register__logIn">Sign in</span>
-//         <img src={decoration} alt="decoration"/>
-//         <form className="Register__form" onSubmit={handleSubmit}>
-//           <div className="Register__data">
-//             <label>
-//               Email
-//               <input
-//               className={!errors.email ? "Register__input" : "Register_error"}
-//               type="email"
-//               name="email"
-//               id="email"
-//               onChange={updateForm}
-//               />
-//             <span className="Register_errorText">{errors.email}</span>
-//             </label>
-//             <label>
-//               Password
-//               <input
-//               className={!errors.password ? "Register__input" : "Register_error"}
-//               type="password"
-//               name="password"
-//               id="password"
-//               onChange={updateForm}
-//               />
-//             <span className="Register_errorText">{errors.password}</span>
-//             </label>
-//             <label>
-//               Repeat password
-//               <input
-//               className={!errors.passwordRep ? "Register__input" : "Register_error"}
-//               type="password"
-//               name="passwordRep"
-//               id="passwordRep"
-//               onChange={updateForm}
-//               />
-//             <span className="Register_errorText">{errors.passwordRep}</span>
-//             </label>
-//           </div>
-//           <div className="Register__btns">
-//             <Link to="/login" className="btn">
-//               Login
-//             </Link>
-//             {!isPending && <button  className="btn">Sign in</button>}
-//             {isPending && <button className="btn" disabled>loading</button>}
-//             {error && <p>{error}</p>}
-//             </div>
-          
-//         </form>
-//       </div>
-//   </div>   
-//   )
-// }
-
-// export default Register;
-
 import React from 'react'
 import HomeHeader from '../Header/HomeHeader';
 import decoration from "../../assets/Decoration.svg";
@@ -146,26 +8,70 @@ import { useSignup } from '../../Hooks/useSignup'
 
 const Register = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [samePassword, setSamePassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [passwordRep, setPasswordRep] = useState('');
   const {signup, isPending, error } = useSignup();
 
-  const handleChangeEmail = (e) => {
-    setEmail(e.target.value);
-  }
-  
-  const handleChangePassword = (e) => {
-    setPassword(e.target.value);
-  }
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    passwordRep: ""
+  });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    passwordRep: ""
+  });
 
-  const handleChangeSamePassword = (e) => {
-    setSamePassword(e.target.value);
-  }
+  const updateForm = e => {
+    const { name, value } = e.target;
+    setForm(prev => {
+      return {
+        ...prev,
+        [name]: value
+      }
+    })
+
+    setErrors(prevState => ({
+      ...prevState,
+      [name]: ""
+    }));
+  };
+  
+  const validate = () => {
+
+    const err = {};
+    const re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+    if (!re.test(form.email)) {
+      err.email = "The email is badly formated!";
+    }
+
+    if (form.password.length < 6) {
+      err.password = "The password is too short!";
+    }
+
+    if (form.passwordRep !== form.password) {
+      err.passwordRep = "The passwords are not the same!";
+    }
+
+    if (Object.values(err).find(e => e.length)) {
+      setErrors(err);
+      return false;
+    }
+    return true;
+  };
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    signup(email, password, samePassword)
+    console.log(validate(), "validate");
+    
+    if (validate()) {
+      console.log(form.email);
+      console.log(form.password);
+    signup(form.email, form.password, form.passwordRep)
+    }
   }
 
   return (
@@ -179,43 +85,40 @@ const Register = () => {
             <label>
               Email
               <input
-              // className={!errors.email ? "Register__input" : "Register_error"}
+              className={!errors.email ? "Register__input" : "Register_error"}
               type="email"
               name="email"
               id="email"
-              value={email}
-              onChange={handleChangeEmail}
+              onChange={updateForm}
               />
-            <span className="Register_errorText"></span>
+            <span className="Register_errorText">{errors.email}</span>
             </label>
             <label>
               Password
               <input
-              // className={!errors.password ? "Register__input" : "Register_error"}
+              className={!errors.password ? "Register__input" : "Register_error"}
               type="password"
               name="password"
               id="password"
-              onChange={handleChangePassword}
+              onChange={updateForm}
               />
-            <span className="Register_errorText"></span>
+            <span className="Register_errorText">{errors.password}</span>
             </label>
             <label>
               Repeat password
               <input
-              // className={!errors.passwordRep ? "Register__input" : "Register_error"}
-              value={samePassword}
+              className={!errors.passwordRep ? "Register__input" : "Register_error"}
               type="password"
               name="passwordRep"
               id="passwordRep"
-              onChange={handleChangeSamePassword}
-              // onChange={updateForm}
+              onChange={updateForm}
               />
-            <span className="Register_errorText"></span>
+            <span className="Register_errorText">{errors.passwordRep}</span>
             </label>
           </div>
           <div className="Register__btns">
             <Link to="/login" className="btn">
-              Login in
+              Login
             </Link>
             {!isPending && <button  className="btn">Sign in</button>}
             {isPending && <button className="btn" disabled>loading</button>}
